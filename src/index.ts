36b874912a973ae539e20ec3bdc9c2352954a4b1
adrian <adriannovella@outlook.es>
2025-01-11 16:07:45 +0100

    faster flipped class add

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 function main() {
-
   // apply flipped class to flipped cards
   const cards = document.querySelectorAll<HTMLDivElement>('.flip')
 
   for (const card of cards) {
-    card.addEventListener('mouseover', () => setTimeout(() => card.classList.add('flipped'), 200))
+    card.addEventListener('mouseover', () => setTimeout(() => card.classList.add('flipped'), 1))
     card.addEventListener('mouseleave', () => card.classList.remove('flipped'))
   }
 }
